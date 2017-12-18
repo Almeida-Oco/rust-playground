f6adacc79aca4832871b3a8c4599285f84651518
Almeida-Oco <intjalmeida@gmail.com>
2017-12-18 04:09:08 +0000

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-First major project on [Rust](https://www.rust-lang.org/en-US/index.html).
-
-Trying to create an easier way to rename files in a system using regex.
-(It is highly likely that such features are already provided by some other program)
+Simple cache like data structure
--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,35 @@
-use std::fs;
-use std::path;
+use std::collections::HashMap;
+use std::collections::hash_map::Entry;
 
-#[allow(unused_variables)]
-
-pub struct Options {
-	path: String,
-	original_fn: Vec<String>,
-	new_fn: String,
+#[allow(dead_code)]
+struct Cacher<T,A,R>
+    where T: Fn(&A) -> R,
+          A: std::cmp::Eq + std::hash::Hash,
+          R: std::clone::Clone,
+{
+	func: T,
+	results: HashMap<A, R>,
 }
-
-impl Options {
-	pub fn new (path: String, new_fn: String) -> Options {
-		let mut vector = Vec::new();
-		Options {path, original_fn: vector, new_fn}
-	}
-
-	pub fn expand_name (&self, name: &String) -> Vec<String> {
-		let mut ret = Vec::new();
-
-		if name.contains("*") {
-			println!("Name contains an '*'");
-			ret = self.expand_ast(name);
-		}
-
-		ret
-	}
+#[allow(dead_code)]
+impl<T, A, R> Cacher<T,A,R>
+    where T: Fn(&A) -> R,
+         A: std::cmp::Eq + std::hash::Hash + std::clone::Clone,
+         R: std::clone::Clone,
+{
+    fn new(func: T) -> Cacher<T, A, R> {
+        let results: HashMap<A,R> = HashMap::new();
+        Cacher {func, results}
+    }
 
 
-	fn expand_ast (&self, pattern: &String) -> Vec<String> {
-		let dir_entries = fs::read_dir(path::Path::new(&self.path)).expect("Failed to open directory!");
-		let names: Vec<String> = dir_entries.filter_map(|entry| {
-			entry.ok().and_then(|dir_entry| {
-				dir_entry.file_name().to_str().map(|name_as_str| {
-					String::from(name_as_str)
-				})
-			})
-		}).collect();
-
-		let mut i = 0;
-		let mut results: Vec<String> = Vec::new();
-		names.iter().for_each(|name| {
-			if name.starts_with(pattern) {
-				results.push(name.clone());	//TODO use something other than clone
+    fn value(&mut self, arg: A) -> R {
+		match self.results.entry(arg) {
+			Entry::Occupied(entry) => entry.get().clone(),
+			Entry::Vacant(entry) => {
+				let ret: R = (self.func)(entry.key());
+				entry.insert(ret.clone());
+				ret
 			}
-		});
-
-		results
-	}
-
-	pub fn get_original_fn (&self) -> &Vec<String> {
-		&self.original_fn
-	}
-
-	pub fn get_new_fn (&self) -> &String {
-		&self.new_fn
-	}
+		}
+    }
 }
